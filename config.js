ad972704631708385ac8b14065d41456cd1c9091
monrtaniegra <mon.reyes@infocap.ai>
2026-06-02 02:00:51 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ window.CONFIG = {
     uploadUrl: "__UPLOAD_URL__",
     statusUrl: "__STATUS_URL__",
     previewUrl: "__PREVIEW_URL__",
+    apiBaseUrl: "__API_BASE_URL__",
     GET_MY_DOCUMENTS_API: "__GET_MY_DOCUMENTS_API__",
     ADMIN_DOCUMENTS_API: "__ADMIN_DOCUMENTS_API__",
     ADMIN_UPDATE_API: "__ADMIN_UPDATE_API__"
